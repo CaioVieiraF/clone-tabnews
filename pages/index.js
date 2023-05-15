3d7c5ce83d35641cf4a25533ea8fbf7d4b6b704c
Caio Vieira <CaioVieiraProfissional@protonmail.com>
2023-05-15 02:07:06 +0000

    Adicionado .editorconfig

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@
 function Home() {
     return (
         <div style={{width: "25rem", border: "1px solid", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", textAlign: "center"}}>
-            <img src={"./eu_e_diulia.jpg"} width={"100%"} />
             <div style={{padding: "1rem"}}>
                 <h3>Schwester, ich liebe dich!</h3>
             </div>
